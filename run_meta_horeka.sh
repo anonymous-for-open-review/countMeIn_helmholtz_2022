6c89dccc36478f8d68dbe7a02c42a7d4010883a5
anonymous-for-open-review <rene.traore@tum.de>
2022-05-20 18:52:38 +0200

    ok for juwels, fix for horeka (account, partition cpuonly etc)

--- run_meta_horeka.sh
+++ run_meta_horeka.sh
@@ -2,29 +2,29 @@
 # `bash -x` for detailed Shell debugging
 
 
-JOBID=$(sbatch --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 run_train_singularity_horeka_v2.sh adaboost grid 0 2>&1 | awk '{print $(NF)}')
+JOBID=$(sbatch --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 run_train_singularity_horeka_v2.sh adaboost grid 0 2>&1 | awk '{print $(NF)}')
 echo "Done with: ${JOBID} - feature eng"
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh adaboost grid 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh adaboost halving 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh gradient_boosting grid 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh gradient_boosting halving 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh voting grid 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh voting halving 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh random_forest grid 1
 
-sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
+sbatch --dependency=singleton --nodes=1 --time=24:00:00 --partition=cpuonly --gres=gpu:1 \
 run_train_singularity_horeka_v2.sh random_forest halving 1
