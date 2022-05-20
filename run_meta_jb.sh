8dc3b37a54b1393e1c2170402ca25aa55a07b2aa
anonymous-for-open-review <rene.traore@tum.de>
2022-05-20 17:36:08 +0200

    add dependency: only do distributed training after step of preprocessing

--- run_meta_jb.sh
+++ run_meta_jb.sh
@@ -4,13 +4,30 @@
 
 ## Build features only
 
-sbatch --nodes=1 --account=hai_countmein --time=24:00:00 --partition=booster --gres=gpu:1 \
-run_train_singularity_jb_v2.sh adaboost grid 0 && \
-run_train_singularity_jb_v2.sh adaboost grid 1 && \
-run_train_singularity_jb_v2.sh adaboost halving 1 && \
-run_train_singularity_jb_v2.sh gradient_boosting grid 1 && \
-run_train_singularity_jb_v2.sh gradient_boosting halving 1 && \
-run_train_singularity_jb_v2.sh voting grid 1 && \
-run_train_singularity_jb_v2.sh voting halving 1 && \
-run_train_singularity_jb_v2.sh random_forest grid 1 && \
+
+JOBID=$(sbatch --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh adaboost grid 0 )
+echo "Done with: ${JOBID} - feature eng"
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh adaboost grid 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh adaboost halving 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh gradient_boosting grid 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh gradient_boosting halving 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh voting grid 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh voting halving 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
+run_train_singularity_jb_v2.sh random_forest grid 1
+
+sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:00:05 --partition=booster --gres=gpu:1 \
 run_train_singularity_jb_v2.sh random_forest halving 1
