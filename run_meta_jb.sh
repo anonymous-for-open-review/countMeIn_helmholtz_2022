c7a7a4d7fc83d8d37806dd84e0da6403f45a12a5
anonymous-for-open-review <rene.traore@tum.de>
2022-05-20 18:16:59 +0200

    replace specific jobid with singleton dependency (sbatch scripting)

--- run_meta_jb.sh
+++ run_meta_jb.sh
@@ -8,18 +8,18 @@
 JOBID=$(sbatch --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh adaboost grid 0 2>&1 | awk '{print $(NF)}')
 echo "Done with: ${JOBID} - feature eng"
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh adaboost grid 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh adaboost grid 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh adaboost halving 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh adaboost halving 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh gradient_boosting grid 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh gradient_boosting grid 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh gradient_boosting halving 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh gradient_boosting halving 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh voting grid 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh voting grid 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh voting halving 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh voting halving 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh random_forest grid 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh random_forest grid 1
 
-sbatch --dependency=afterok:${JOBID} --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh random_forest halving 1
+sbatch --dependency=singleton --nodes=1 --account=hai_countmein --time=00:10:00 --partition=booster --gres=gpu:1 run_train_singularity_jb_v2.sh random_forest halving 1
